b6e0ecff518e7c239f0b850f54d2482d62766719
Sifem Macar <sifemmacar@gmail.com>
2024-04-15 18:09:03 +0200

    added select silhouette

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,9 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def select_silhouette
+    @silhouettes = Silhouette.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  post "select" => "user_silhouettes#select", as: :select
-  get "generate" => "user_silhouettes#generate", as: :generate
-  get "show" => "user_silhouettes#show", as: :user_silhouettes_show
-  patch "twosilhouettes" => "user_silhouettes#upload_back_photo", as: :upload_photo
-  post "process_payment" => "users#process_payment", as: :process_payment
-  get "payment" => "users#payment", as: :payment
-  get "show" => "users#payment", as: :user_show
+  
+
+
+  get "select_silhouette", to: "pages#select_silhouette"
+
+
+
+
+
+  # get "generate" => "user_silhouettes#generate", as: :generate
+  # get "show" => "user_silhouettes#show", as: :user_silhouettes_show
+  # patch "twosilhouettes" => "user_silhouettes#upload_back_photo", as: :upload_photo
+  # post "process_payment" => "users#process_payment", as: :process_payment
+  # get "payment" => "users#payment", as: :payment
+  # get "show" => "users#payment", as: :user_show
 end
