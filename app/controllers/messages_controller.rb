14096254fc1276ead51d27176ad4e4fc6f2e30d2
Lee Mckenna <kenna12345@hotmail.co.uk>
2024-05-02 09:50:18 +0200

    AI prompt changed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -60,7 +60,7 @@ class MessagesController < ApplicationController
     base = "I have a #{ai_silhouette.neutral_silhouette} silhouette
     and would like some clothing recommendations. I got this recommendation
     #{current_user.silhouettes.first.recommendations.first.description}
-    And my question is #{user_content}. If the question is about another topic, say you only give advise about fashion in one sentence, and absolutely don't give any fashion advice at all. Respond as if you are my best friend. Address me using words like honey, darling, sweetheart, gorgeous etc. You may use the book “The Triumph of Individual Style: A Guide to Dressing Your Body, Your Beauty, Your Self Paperback – October 1, 2002
+    And my question is #{user_content}. If the question is about another topic, say you only give advise about fashion in one sentence, and absolutely don't give any but fashion advice. Respond as if you are my best friend. Address me using words like honey, darling, sweetheart, gorgeous, babe etc. You may use the book “The Triumph of Individual Style: A Guide to Dressing Your Body, Your Beauty, Your Self Paperback – October 1, 2002
     by Carla Mason Mathis(Author),Helen Villa Connor(Author), but don't mention it to me. Sound like a fashionista, chic and creative."
     chaptgpt_response = client.chat(parameters: { model: "gpt-3.5-turbo", messages: [{ role: "user", content: base }] })
     chaptgpt_response["choices"][0]["message"]["content"]
